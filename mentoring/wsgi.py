2a2bedbe2a28b05b32f7c2a9b138052b9f3ddfd5
Matt Johnson <mdj2@pdx.edu>
2014-01-03 14:09:32 -0800

    Upgrade to Django 1.6

--- mentoring/wsgi.py
+++ mentoring/wsgi.py
@@ -17,8 +17,19 @@ import os
 import site
 import sys
 
+# Remember original sys.path.
+prev_sys_path = list(sys.path)
+
 root = os.path.normpath(os.path.join(os.path.dirname(__file__), "../"))
 sys.path.append(root)
+
+# Reorder sys.path so new directories at the front.
+new_sys_path = [] 
+for item in list(sys.path): 
+    if item not in prev_sys_path: 
+        new_sys_path.append(item) 
+        sys.path.remove(item) 
+sys.path[:0] = new_sys_path 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mentoring.settings")
 
