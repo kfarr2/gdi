b38c6b9f26b6b720417582a57fb351a0d8bbcb8b
Konstantin <kfarr2@pdx.edu>
2015-01-06 18:53:08 -0800

    Change import class.views to import views as class

--- mentoring/urls.py
+++ mentoring/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
-import views
-import surveys.views
-import matches.views
-import surveyadmin.views
+#import views
+from mentoring import views as views
+from mentoring.surveys import views as surveys
+from mentoring.matches import views as matches
+from mentoring.surveyadmin import views as surveyadmin
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -12,34 +13,34 @@ urlpatterns = patterns('',
     url(r'^$', views.home),
 
     # surveys
-    url(r'^surveys/mentor/?$', surveys.views.mentor, name='surveys-mentor'),
-    url(r'^surveys/mentee/?$', surveys.views.mentee, name='surveys-mentee'),
-    url(r'^surveys/done/?$', surveys.views.done, name='surveys-done'),
-    url(r'^surveys/response/(\d+)/?$', surveys.views.response, name='surveys-response'),
-    url(r'^surveys/report/(\d+)/?$', surveys.views.report, name='surveys-report'),
+    url(r'^surveys/mentor/?$', surveys.mentor, name='surveys-mentor'),
+    url(r'^surveys/mentee/?$', surveys.mentee, name='surveys-mentee'),
+    url(r'^surveys/done/?$', surveys.done, name='surveys-done'),
+    url(r'^surveys/response/(\d+)/?$', surveys.response, name='surveys-response'),
+    url(r'^surveys/report/(\d+)/?$', surveys.report, name='surveys-report'),
 
     # matches
-    url(r'^matches/marry/?$', matches.views.marry, name='matches-marry'),
-    url(r'^matches/divorce/?$', matches.views.divorce, name='matches-divorce'),
-    url(r'^matches/engage/?$', matches.views.engage, name='matches-engage'),
-    url(r'^matches/breakup/?$', matches.views.breakup, name='matches-breakup'),
-    url(r'^matches/complete/?$', matches.views.complete, name='matches-complete'),
-    url(r'^matches/report/?$', matches.views.report, name='matches-report'),
+    url(r'^matches/marry/?$', matches.marry, name='matches-marry'),
+    url(r'^matches/divorce/?$', matches.divorce, name='matches-divorce'),
+    url(r'^matches/engage/?$', matches.engage, name='matches-engage'),
+    url(r'^matches/breakup/?$', matches.breakup, name='matches-breakup'),
+    url(r'^matches/complete/?$', matches.complete, name='matches-complete'),
+    url(r'^matches/report/?$', matches.report, name='matches-report'),
     # mentors and mentee administration
-    url(r'^(mentors)/delete/?$', matches.views.remove, name='mentors-delete'),
-    url(r'^(mentees)/delete/?$', matches.views.remove, name='mentees-delete'),
+    url(r'^(mentors)/delete/?$', matches.remove, name='mentors-delete'),
+    url(r'^(mentees)/delete/?$', matches.remove, name='mentees-delete'),
 
     # management
-    url(r'^manage/?$', matches.views.manage, name='manage'),
-    url(r'^manage/match/?$', matches.views.match, name='manage-match'),
-    url(r'^manage/ments/?$', matches.views.ments, name='manage-ments'),
-    url(r'^manage/completions/?$', matches.views.completions, name='manage-completions'),
-    url(r'^manage/settings/?$', matches.views.settings, name='manage-settings'),
+    url(r'^manage/?$', matches.manage, name='manage'),
+    url(r'^manage/match/?$', matches.match, name='manage-match'),
+    url(r'^manage/ments/?$', matches.ments, name='manage-ments'),
+    url(r'^manage/completions/?$', matches.completions, name='manage-completions'),
+    url(r'^manage/settings/?$', matches.settings, name='manage-settings'),
 
     # admin
-    url(r'^sadmin/?$', surveyadmin.views.admin, name='admin-surveys'),
-    url(r'^sadmin/survey/(\d+)?$', surveyadmin.views.questions, name='admin-surveys-questions'),
-    url(r'^sadmin/question/(\d+)?$', surveyadmin.views.question, name='admin-surveys-question'),
+    url(r'^sadmin/?$', surveyadmin.admin, name='admin-surveys'),
+    url(r'^sadmin/survey/(\d+)?$', surveyadmin.questions, name='admin-surveys-questions'),
+    url(r'^sadmin/question/(\d+)?$', surveyadmin.question, name='admin-surveys-question'),
 
 
     # auth
