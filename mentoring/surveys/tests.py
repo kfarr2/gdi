a761330c83ef7114cd3cccb3e9b728e247343106
Konstantin <kfarr2@pdx.edu>
2015-01-08 18:54:20 -0800

    Write tests for surveys

--- mentoring/surveys/tests.py
+++ mentoring/surveys/tests.py
@@ -1,16 +1,33 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
 
-Replace this with more appropriate tests for your application.
+This file should test everything having to do with the Surveys application.
+It will not contain tests for models as they will be handled in the utils.py
+file.
+Written by Konstantin, but if anything goes wrong, it was Sean.
+
 """
 
 from django.test import TestCase
+from mentoring.utils.tests import MentoringBaseTest
+from .forms import SurveyForm
+from .models import Survey
 
+#TODO: finish survey forms & views tests
+class SurveyFormsTest(MentoringBaseTest):
+    """
+    Test all forms in the surveys app
+    """
+    def setUp(self):
+        super(SurveyFormsTest, self).setUp()
+        self.client.login(username=self.admin.username, password=self.admin.password)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_invalid_survey_form(self):
+        form = SurveyForm(survey=self.survey)
+        self.assertFalse(form.is_valid())
+
+    #TODO: finish this test
+    def test_valid_survey_form(self):
+        count = Survey.objects.count()
+        form = SurveyForm(
+            survey=self.survey,
+        )
