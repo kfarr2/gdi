eaf2d0b2cdfde6bf2142272fbeaf45b97d8d318c
Konstantin <kfarr2@pdx.edu>
2015-01-15 19:03:43 -0800

    Begin adding tests for surveyadmin

--- mentoring/surveyadmin/tests.py
+++ mentoring/surveyadmin/tests.py
@@ -4,13 +4,17 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
+from unittest import mock
+from unittest.mock import patch
+from model_mommy.mommy import make
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.conf import settings
+from django.contrib.auth.models import User, Permission
 from django.test import TestCase
+from mentoring.utils.tests import MentoringBaseTest
+from mentoring.matches.models import Mentor, Mentee
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
