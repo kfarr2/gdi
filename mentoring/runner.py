226bd7284c71a7d1293f45db833aa2d6145c32db
Konstantin <kfarr2@pdx.edu>
2015-01-21 20:45:27 -0800

    Add custom test runner

---
+++ mentoring/runner.py
@@ -0,0 +1,15 @@
+import tempfile
+import shutil
+from django.conf import settings
+from django.test.runner import DiscoverRunner
+
+class GDIRunner(DiscoverRunner):
+    """
+    Creates a temporary directory, runs the tests in there, then deletes it
+    """
+    def run_tests(self, *args, **kwargs):
+        settings.TEST = True
+        settings.MEDIA_ROOT = tempfile.mkdtemp(prefix="gdi_unit_test_tmp_dir_")
+        super(MLPRunner, self).run_tests(*args, **kwargs)
+        shutil.rmtree(settings.MEDIA_ROOT)
+        settings.TEST = False
